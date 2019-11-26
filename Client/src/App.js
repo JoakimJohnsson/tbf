701edee06f05dd22b0c7af03bc759090594f05a4
JoakimJohnsson <joakim.johnsson@su.se>
2019-11-26 15:01:19 +0100

    blog project files

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,15 +3,16 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-class App extends Component {
+// https://jsonplaceholder.typicode.com/posts
 
+class App extends Component {
     state = {
         hello: null
     };
 
     componentDidMount() {
-        axios.get('/hello')
-            .then(res => this.setState({hello: res.data}))
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => console.log("RES DATA: ", res.data))
             .catch(err => console.log(err))
     }
 
